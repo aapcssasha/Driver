b2688e23f6f37f827387bac8a8d65a0df8d2903e
apcssasha <92939435+apcssasha@users.noreply.github.com>
2023-05-17 02:32:23 -0400

    New website

---
+++ script.js
@@ -0,0 +1,36 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var dataArray = [
+        ['Stock', 'Percentage'],
+        ['BABA', 20],
+        ['AAPL', 21],
+        ['TSLA', 20],
+        ['MSFT', 25],
+        ['COF', 8.24],
+        ['LUV', 7.57],
+        ['Cash', 20]
+    ];
+
+    dataArray.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    var data = google.visualization.arrayToDataTable(dataArray);
+
+    var options = {
+        width: 400,
+        height: 300,
+        backgroundColor: '#808080',
+        legend: {textStyle: {color: 'white'}},
+        pieSliceBorderColor: '#808080',
+        // move the graph to the left
+        chartArea: {right:0, top: 0, width: '100%', height: '50%'},
+
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
