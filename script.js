f0bf532c8ac7e7ca52aef835eba5051b4304ec6e
apcssasha <92939435+apcssasha@users.noreply.github.com>
2023-05-17 12:05:02 -0400

    revert changes, no stock prices

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-// Define the symbols of your stocks
-let stockSymbols = ['BABA', 'AAPL', 'TSLA', 'MSFT', 'COF', 'LUV'];
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(updateDataArray);
 
-// You need to get a free API key from Alpha Vantage
-const apiKey = "ETUXG97GLN4PVQ2R"; 
-
-async function getPrice(symbol) {
-    let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    return parseFloat(data['Global Quote']['05. price']);
-}
-
-async function updateDataArray() {
+function drawChart() {
     var dataArray = [
         ['Stock', 'Percentage'],
-        ['BABA', await getPrice('BABA')],
-        ['AAPL', await getPrice('AAPL')],
-        ['TSLA', await getPrice('TSLA')],
-        ['MSFT', await getPrice('MSFT')],
-        ['COF', await getPrice('COF')],
-        ['LUV', await getPrice('LUV')],
+        ['BABA', 20],
+        ['AAPL', 21],
+        ['TSLA', 20],
+        ['MSFT', 25],
+        ['COF', 8.24],
+        ['LUV', 7.57],
         ['Cash', 20]
     ];
 
@@ -35,24 +25,12 @@ async function updateDataArray() {
         backgroundColor: '#f1efef',
         legend: {textStyle: {color: 'black'}},
         pieSliceBorderColor: '#000',
+        // move the graph to the left
         chartArea: { width: '100%', height: '50%'},
+
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
     chart.draw(data, options);
 }
-
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(updateDataArray);
-
-// Iterate over your stock symbols to fetch prices and update the corresponding HTML elements
-stockSymbols.forEach(symbol => {
-    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        let stockPrice = parseFloat(data["Global Quote"]["05. price"]).toFixed(2);
-        document.getElementById(`price-${symbol}`).innerText = stockPrice;
-    })
-    .catch(error => console.error('Error:', error));
-});
