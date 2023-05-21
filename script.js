7f0e87199ca55c269c08d20bb95a3ad04c6e0204
apcssasha <92939435+apcssasha@users.noreply.github.com>
2023-05-21 06:08:44 -0400

    delete login buttoms and add article 4//20/2023

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
+
 window.addEventListener('resize', drawChart);
 
 function drawChart() {
@@ -43,32 +44,3 @@ function drawChart() {
 
     chart.draw(data, options);
 }
-let modal = document.getElementById("loginModal");
-let btn = document.getElementById("login-button");
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// When the user clicks on the "Submit" button, log the entered data and close the modal
-document.getElementById('submit-login').addEventListener('click', function() {
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    console.log('Logging in with username:', username, 'and password:', password);
-    modal.style.display = "none";
-});
-
