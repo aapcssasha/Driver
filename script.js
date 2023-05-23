5afd0b8ee9e47a8c9f29a0f48d61e99afbd1e584
apcssasha <92939435+apcssasha@users.noreply.github.com>
2023-05-22 23:41:38 -0400

    new graph of the SPY

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(function() {
+    drawChart();
+    drawSPYChart();
+});
 
-
-window.addEventListener('resize', drawChart);
+window.addEventListener('resize', function() {
+    drawChart();
+    drawSPYChart();
+});
 
 function drawChart() {
     let portfolioSection = document.getElementById('portfolio-section');
@@ -44,3 +49,58 @@ function drawChart() {
 
     chart.draw(data, options);
 }
+function drawSPYChart() {
+    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=SPY&apikey=02J3FNJRUE6Y1DFR')
+    .then(response => response.json())
+    .then(data => {
+        let dataTable = [];
+        let dailyData = data['Time Series (Daily)'];
+
+        for (let date in dailyData) {
+            let parsedDate = new Date(date);
+            parsedDate = new Date(parsedDate.getFullYear(), parsedDate.getMonth(), parsedDate.getDate());
+            let closePrice = parseFloat(dailyData[date]['4. close']);
+
+            console.log(parsedDate instanceof Date); // Debugging line
+
+            if (!isNaN(closePrice)) {
+                dataTable.push([parsedDate, closePrice]);
+            } else {
+                console.log(`Invalid data for date ${date}: ${dailyData[date]['4. close']}`);
+            }
+        }
+        dataTable.reverse();
+        dataTable.unshift(['Date', 'Close']); // Add column headers after reversing the data.
+
+        console.log(dataTable.slice(0,5)); // Debugging line
+
+        var data;
+        try {
+            data = google.visualization.arrayToDataTable(dataTable, false); // Added false for treating first row as data.
+        } catch (error) {
+            console.error('Error converting data to DataTable:', error);
+            console.log('dataTable:', dataTable);
+        }
+
+        var options = {
+            title: 'SPY Price',
+            legend: { position: 'bottom' },
+            hAxis: { title: 'Date', format: 'M/d/yy' },
+            vAxis: { title: 'Price' },
+        };
+
+        if (data) {
+            var chart = new google.visualization.LineChart(document.getElementById('spy_chart_div'));
+            chart.draw(data, options);
+        }
+    })
+    .catch(err => console.error(err));
+}
+
+
+
+
+
+
+
+
