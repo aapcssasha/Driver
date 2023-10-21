8b110b1f0f8903ba7a28bbb075af6593415cba1a
Alejandro Pina <alejandropina@AlejandrosAir2.attlocal.net>
2023-10-21 18:22:13 -0400

    score and spy plotting

--- script.js
+++ script.js
@@ -1,87 +1,93 @@
-let spyChartData;
+const convertDate = (date) => {
+  const [month, day, year] = date.split('/');
+  return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+}
 
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(fetchSPYData);
 
-window.addEventListener('resize', function() {
-    drawChart();
-    if (spyChartData) {
-        drawSPYChart();
+// Prepare empty arrays to store data
+let spyDates = [];
+let spyCloses = [];
+let econDates = [];
+let econScores = [];
+
+// Fetching SPY data
+fetch('https://raw.githubusercontent.com/aapcssasha/Driver/main/spy_data.txt')
+.then(response => response.text())
+.then(data => {
+  const lines = data.trim().split('\n');
+  lines.shift(); // remove header
+
+  lines.forEach(line => {
+    const [date, close] = line.split('\t');
+    spyDates.push(date);
+    spyCloses.push(parseFloat(close));
+  });
+
+  // Fetching Economic Scores
+  return fetch('https://raw.githubusercontent.com/aapcssasha/Driver/main/economic_scores.txt');
+})
+.then(response => response.text())
+.then(data => {
+  const lines = data.trim().split('\n');
+
+  lines.forEach(line => {
+    const [rawDate, scoreText] = line.split(' - economic score : ');
+    const date = convertDate(rawDate);
+    // Only push to econDates and econScores if date exists in spyDates
+    if (spyDates.includes(date)) {
+      econDates.push(date);
+      econScores.push(parseFloat(scoreText));
     }
-});
+  });
 
-function drawChart() {
-    let portfolioSection = document.getElementById('portfolio-section');
-    let portfolioWidth = portfolioSection.offsetWidth;
-
-    const dataArray = [
-        ['Stock', 'Percentage'],
-        ['LUV', 2670],
-        ['COF', 2860],
-        ['BABA', 9120],
-        ['THO', 3000],
-        ['NIU', 2360],
-        ['TUP', 1300],
-        ['SPWH', 2640],
-        ['HBI', 2330],
-        ['VZ', 3590],
-        ['FINV', 2110],
-        ['M', 2580],
-        ['Cash', 170]
-    ];
-
-    dataArray.sort(function(a, b) {
-        return b[1] - a[1];
-    });
-
-    let data = google.visualization.arrayToDataTable(dataArray);
-
-    let options = {
-        width: portfolioWidth,
-        height: 400,
-        backgroundColor: '#f1efef',
-        legend: {textStyle: {color: 'black', fontSize: 12}},
-        pieSliceBorderColor: '#000',
-        chartArea: { width: '100%', height: '65%' },
-        pieSliceTeXt: 'Percentage',
-    };
-
-    let chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-    chart.draw(data, options);
-}
-
-function drawSPYChart() {
-    let dates = spyChartData.map(dataPoint => dataPoint[0]);
-    let prices = spyChartData.map(dataPoint => dataPoint[1]);
-
-    var ctx = document.getElementById('spy_chart_div').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: 'SPY Price',
-                borderColor: 'rgb(255, 99, 132)',
-                data: prices
-            }]
+  // Create Chart
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: spyDates, // make sure spyDates and econDates align if necessary
+      datasets: [{
+        label: 'SPY Close Prices',
+        data: spyCloses,
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+        yAxisID: 'y'
+      }, {
+        label: 'Economic Scores',
+        data: econScores,
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+        yAxisID: 'y1'
+      }]
+    },
+    options: {
+      scales: {
+        x: {
+          beginAtZero: true
         },
-        options: {
-            scales: {
-                x: {
-                    type: 'time',
-                    title: {
-                        display: true,
-                        text: 'Date'
-                    }
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Price'
-                    }
-                }
-            }
+        y: {
+          type: 'linear',
+          position: 'left',
+          title: {
+            display: true,
+            text: 'SPY Close Prices'
+          }
+        },
+        y1: {
+          type: 'linear',
+          position: 'right',
+          grid: {
+            drawOnChartArea: false, // ensure it doesn't interfere with main y axis
+          },
+          title: {
+            display: true,
+            text: 'Economic Scores'
+          }
         }
-    });
-}
+      }
+    }
+  });
+})
+.catch(error => {
+  console.error("Something went wrong:", error);
+});
