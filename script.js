a201ed5f8342dc562971d877cff4f4e1b3b304fa
Alejandro Pina <alejandropina@AlejandrosAir2.attlocal.net>
2023-10-29 14:45:23 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("Chart.js loaded:", typeof Chart !== "undefined");
-console.log("Moment.js loaded:", typeof moment !== "undefined");
+
 
 const convertDate = (date) => {
   const [month, day, year] = date.split("/");
@@ -101,106 +100,6 @@ fetch("https://raw.githubusercontent.com/aapcssasha/Driver/main/spy_data.txt")
   })
   .catch((error) => {
     console.error("Something went wrong:", error);
-  });
-
-// Fetch the data from the GitHub repo
-fetch(
-  "https://raw.githubusercontent.com/aapcssasha/Driver/main/data/my_data.txt"
-)
-  .then((response) => {
-    console.log("Initial fetch response:", response);
-    return response.text();
-  })
-  .then((data) => {
-    console.log("Fetched raw data:", data); // Check the raw data
-
-    // Parsing the fetched data
-    const rows = data.split("\n");
-    const labels = [],
-      vixData = [];
-
-    for (let i = 1; i < rows.length; i++) {
-      const cells = rows[i].split("\t");
-      labels.push(cells[0]);
-      vixData.push(parseFloat(cells[1]));
-    }
-
-    console.log("Parsed Labels and VIX Data:", labels, vixData); // Check parsed data
-
-    async function fetchAndPlotVIX() {
-      console.log("fetchAndPlotVIX started");
-      console.log(document.getElementById("VIX")); // New line here
-      const response = await fetch(
-        "https://raw.githubusercontent.com/aapcssasha/Driver/main/data/my_data.txt"
-      );
-      const text = await response.text();
-      console.log("Fetched VIX Data:", text);
-
-      // Parse the data to separate the dates and close values
-      const lines = text.split("\n").slice(1); // Skip the header
-      const dates = [];
-      const closeValues = [];
-
-      lines.forEach((line) => {
-        const [date, close] = line.split("\t");
-        if (date && close) {
-          dates.push(date);
-          closeValues.push(parseFloat(close));
-        }
-      });
-      console.log("Parsed Dates and Close Values:", dates, closeValues);
-      console.log("Creating VIX Chart with:", dates, closeValues);
-
-      // Create the chart
-      const ctx = document.getElementById("VIX").getContext("2d");
-      const myChart = new Chart(ctx, {
-        type: "line",
-        data: {
-          labels: dates,
-          datasets: [
-            {
-              label: "Close Values",
-              data: closeValues,
-              borderColor: "rgba(75, 192, 192, 1)",
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            x: {
-              type: "time",
-              time: {
-                parser: "YYYY-MM-DD",
-                unit: "day",
-                displayFormats: {
-                  day: "MMM D",
-                },
-                tooltipFormat: "ll",
-              },
-              title: {
-                display: true,
-                text: "Date",
-              },
-            },
-            y: {
-              title: {
-                display: true,
-                text: "Value",
-              },
-            },
-          },
-        },
-      });
-    }
-    console.log(
-      "Date Format Verification:",
-      moment(dates[0], "YYYY-MM-DD").isValid()
-    ); // New line here
-    fetchAndPlotVIX();
-  })
-  .catch((error) => {
-    console.error("Something went wrong with fetching VIX:", error); // Catch any errors
   });
 
 async function fetchLatestScore() {
